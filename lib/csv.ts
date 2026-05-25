63ee095709ec60b18db9c946931008532958184d
Ishmael Harry-Deckor <ishmaelharrydeckor@gmail.com>
2026-05-25 00:46:18 +0000

    fix: create missing csv utility file

---
+++ lib/csv.ts
@@ -0,0 +1,14 @@
+// lib/csv.ts
+
+export function downloadToCSV(csvStr: string, filename: string) {
+  const blob = new Blob([csvStr], { type: 'text/csv;charset=utf-8;' });
+  const url = URL.createObjectURL(blob);
+  
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', `${filename}.csv`);
+  
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
